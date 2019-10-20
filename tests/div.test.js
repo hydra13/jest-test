5fe6c70f029e1464521e4ca1f6624d1a8d69d58a
Hydra13 <rob.sabirov@yandex.ru>
2019-10-20 09:53:24 +0300

    added the addition test for div

--- tests/div.test.js
+++ tests/div.test.js
@@ -1,5 +1,12 @@
 const div = require('../src/div');
 
-test('(8, 4) - result should be 2', () => {
-    expect( div(8, 4) ).toBe(2);
+describe('testing division', () => {
+    test('(8, 4) - result should be 2', () => {
+        expect( div(8, 4) ).toBe(2);
+    })
+    
+    test('(8, 0) - throw error "Error: Division by zero."', () => {
+        expect( div(8, 0) ).toThrow('Error: Division by zero.');
+    })
 })
+
